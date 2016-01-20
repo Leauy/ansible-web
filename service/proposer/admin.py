43d5fa1eeab9b4f12a3f25565908e1830b4d0351
liuyang <liuyang11@Config550VM0.(none)>
2016-01-20 17:08:38 +0800

    delete

--- service/proposer/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from proposer.models import Service,ServiceConfig,Hosts,PlayBooks,Log,Task
-# Register your models here.
-admin.site.register(Service)
-admin.site.register(ServiceConfig)
-admin.site.register(Hosts)
-admin.site.register(PlayBooks)
-admin.site.register(Log)
-admin.site.register(Task)
